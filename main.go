e0b8ce70b63be134bd24402e66c6cb27386969a2
Lucas Pape <lucaspape2002@protonmail.com>
2022-08-11 00:40:32 +0200

    Reuse objects

--- main.go
+++ main.go
@@ -13,98 +13,143 @@ import (
 	"github.com/g3n/engine/math32"
 	"github.com/g3n/engine/renderer"
 	"github.com/g3n/engine/window"
+	"strconv"
+	"sync"
 	"time"
 )
 
+var n int32 = 100000
+
+var threads = 4
+
+var steps float32 = 100
+var step float32 = 1
+
+var wg sync.WaitGroup
+
 func main() {
-	// Create application and scene
 	a := app.App()
 	scene := core.NewNode()
 
-	// Set the scene to be managed by the gui manager
 	gui.Manager().Set(scene)
 
-	// Create perspective camera
 	cam := camera.New(1)
 	cam.SetPosition(0, 0, 70)
 	scene.Add(cam)
 
-	// Set up orbit control for the camera
 	camera.NewOrbitControl(cam)
 
-	// Set up callback to update viewport and camera aspect ratio when the window is resized
 	onResize := func(evname string, ev interface{}) {
-		// Get framebuffer size and update viewport accordingly
 		width, height := a.GetSize()
 		a.Gls().Viewport(0, 0, int32(width), int32(height))
-		// Update the camera's aspect ratio
 		cam.SetAspect(float32(width) / float32(height))
 	}
 	a.Subscribe(window.OnWindowSize, onResize)
 	onResize("", nil)
 
-	// Create and add lights to the scene
-	scene.Add(light.NewAmbient(&math32.Color{1.0, 1.0, 1.0}, 0.8))
-	pointLight := light.NewPoint(&math32.Color{1, 1, 1}, 5.0)
+	scene.Add(light.NewAmbient(&math32.Color{R: 1.0, G: 1.0, B: 1.0}, 0.8))
+	pointLight := light.NewPoint(&math32.Color{R: 1, G: 1, B: 1}, 5.0)
 	pointLight.SetPosition(1, 0, 80)
 	scene.Add(pointLight)
 
-	// Set background color to gray
 	a.Gls().ClearColor(0, 0, 0, 1)
 
+	sMat := material.NewStandard(&math32.Color{
+		R: 0,
+		G: 0,
+		B: 255,
+	})
+
+	vectors := lorenzAttractor(n)
 	var spheres []*graphic.Mesh
 
-	l := lorenzAttractor(30000)
+	s := geometry.NewSphere(0.01, 10, 10)
 
-	for _, v := range l {
-		sMat := material.NewStandard(&math32.Color{
-			R: 0,
-			G: 0,
-			B: 255,
-		})
-
-		s := geometry.NewSphere(0.01, 10, 10)
+	for _, v := range vectors {
 		mesh := graphic.NewMesh(s, sMat)
+
 		mesh.SetPosition(v.X, v.Y, v.Z)
 		scene.Add(mesh)
 
 		spheres = append(spheres, mesh)
 	}
 
-	var steps float32
-	var step float32
+	chunks := chunkSlice(spheres, len(spheres)/threads)
+	vectorChunks := chunkSlice(vectors, len(spheres)/threads)
 
-	steps = 100
-	step = 1
+	lastFrameTime := time.Now().UnixNano()
+	frames := 0
 
-	// Run the application
+	fpsText := gui.NewLabel("0 fps")
+	fpsText.SetPosition(10, 10)
+	fpsText.SetSize(40, 40)
+	scene.Add(fpsText)
+
+	frameTimeText := gui.NewLabel("")
+	frameTimeText.SetPosition(10, 40)
+	frameTimeText.SetSize(40, 40)
+	scene.Add(frameTimeText)
+
 	a.Run(func(renderer *renderer.Renderer, deltaTime time.Duration) {
 		a.Gls().Clear(gls.DEPTH_BUFFER_BIT | gls.STENCIL_BUFFER_BIT | gls.COLOR_BUFFER_BIT)
+
+		frameTimeText.SetText(strconv.FormatInt(deltaTime.Milliseconds(), 10) + " ms/frame")
+
+		if (time.Now().UnixNano()-lastFrameTime)/1000000000.0 >= 1 {
+			fpsText.SetText(strconv.Itoa(frames) + " fps")
+
+			frames = 0
+			lastFrameTime = time.Now().UnixNano()
+		}
 
 		if step >= steps {
 			step = 1
 
-			for i, s := range spheres {
-				p := l[i]
-				s.SetPosition(p.X, p.Y, p.Z)
-			}
-		}
-
-		for i, s := range spheres {
-			if i+1 >= len(spheres) {
-				continue
+			for i, chunk := range chunks {
+				wg.Add(1)
+				go resetAnimation(vectorChunks[i], chunk)
 			}
 
-			p := l[i]
-			np := l[i+1]
-
-			s.SetPosition(p.X+((np.X-p.X)*((1/steps)*step)), p.Y+((np.Y-p.Y)*((1/steps)*step)), p.Z+((np.Z-p.Z)*((1/steps)*step)))
+			wg.Wait()
 		}
+
+		for i, chunk := range chunks {
+			wg.Add(1)
+			go animate(vectorChunks[i], chunk)
+		}
+
+		wg.Wait()
 
 		step++
 
-		renderer.Render(scene, cam)
+		_ = renderer.Render(scene, cam)
+
+		frames++
 	})
+}
+
+func resetAnimation(vectors []math32.Vector3, spheres []*graphic.Mesh) {
+	for i, s := range spheres {
+		p := vectors[i]
+		s.SetPosition(p.X, p.Y, p.Z)
+	}
+
+	wg.Done()
+}
+
+func animate(vectors []math32.Vector3, spheres []*graphic.Mesh) {
+	for i, s := range spheres {
+		if i+1 >= len(spheres) {
+			continue
+		}
+
+		p := vectors[i]
+		np := vectors[i+1]
+
+		s.SetPosition(p.X+((np.X-p.X)*((1/steps)*step)), p.Y+((np.Y-p.Y)*((1/steps)*step)), p.Z+((np.Z-p.Z)*((1/steps)*step)))
+	}
+
+	wg.Done()
 }
 
 func lorenzAttractor(n int32) []math32.Vector3 {
@@ -152,4 +197,21 @@ func lorenzAttractor(n int32) []math32.Vector3 {
 	}
 
 	return v
+}
+
+func chunkSlice[Type comparable](slice []Type, chunkSize int) [][]Type {
+	var chunks [][]Type
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
 }
